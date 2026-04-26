b02d9d276feea10d0f70bfc81efe8f886d55f927
chownrani <chownrani@proton.me>
2026-04-26 03:25:14 -0300

    new architecture

--- src/repositories/taskRepository.ts
+++ src/repositories/taskRepository.ts
@@ -1,6 +1,6 @@
 import type { Task } from "../domain/generated/prisma/browser";
 import type { TaskCreateInput } from "../domain/generated/prisma/models";
-import { prisma } from "./database";
+import { prisma } from "../database/database";
 
 export class TaskRepository {
     async createTask(task: TaskCreateInput): Promise<Task> {
