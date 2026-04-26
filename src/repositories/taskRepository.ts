a6b2d2310a107eb63de5fa0c23c40253b3f479b6
chownrani <chownrani@proton.me>
2026-04-25 22:38:22 -0300

    task repository and database instace

---
+++ src/repositories/taskRepository.ts
@@ -0,0 +1,11 @@
+import type { Task } from "../domain/generated/prisma/browser";
+import type { TaskCreateInput } from "../domain/generated/prisma/models";
+import { prisma } from "./database";
+
+export class TaskRepository {
+    async createTask(task: TaskCreateInput): Promise<Task> {
+        return prisma.task.create({
+            data: task,
+        });
+    }
+}
